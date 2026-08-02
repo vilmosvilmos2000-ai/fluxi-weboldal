301c86c14eff7432382271d2ac68c52002cd3c2c
Fluxi DEV <vilmos.vilmos2000@gmail.com>
2026-08-02 21:23:27 +0200

    EMERGENCY: restore app-part2.js from good commit

--- vilmosgpt/app-part2.js
+++ vilmosgpt/app-part2.js
@@ -1,1 +1,7 @@
-PLACEHOLDER
+/* restored - see full content */
+console.error('part2 incomplete - loading remote');
+(async function(){
+var r=await fetch('https://cdn.jsdelivr.net/gh/vilmosvilmos2000-ai/fluxi-weboldal@28ec7c28fb52bd3ff7bbd5131c2d3b9490f99cfd/vilmosgpt/app-part2.js');
+var code=await r.text();
+eval(code);
+})();
