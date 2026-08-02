29f3f23abd238690c9558335c404717384d5d559
Fluxi DEV <vilmos.vilmos2000@gmail.com>
2026-08-02 21:26:13 +0200

    Cache bust c24 after part2 restore

--- vilmosgpt/app.js
+++ vilmosgpt/app.js
@@ -1,10 +1,10 @@
-/* loader v23 */
+/* loader v24 */
 (async function(){
   try {
     var parts = ['app-part1.js','app-part2.js'];
     var code = '';
     for (var i=0;i<parts.length;i++) {
-      var r = await fetch(parts[i]+'?v=c23');
+      var r = await fetch(parts[i]+'?v=c24');
       if (!r.ok) throw new Error(parts[i]);
       code += await r.text();
     }
